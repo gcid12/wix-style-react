25f8fac8338bd6f81b6f945258d7a0c076a2e348
TalZa <talza@wix.com>
2020-07-19 20:23:49 +0300

    remove irrelevant tests and uni driver functions

--- src/Animate/test/Animate.spec.js
+++ src/Animate/test/Animate.spec.js
@@ -7,31 +7,10 @@ import { animatePrivateDriverFactory } from './Animate.private.uni.driver';
 describe(Animate.displayName, () => {
   const render = createRendererWithUniDriver(animatePrivateDriverFactory);
 
-  afterEach(() => {
-    cleanup();
-  });
+  afterEach(cleanup);
 
   it('should render', async () => {
     const { driver } = render(<Animate />);
-
     expect(await driver.exists()).toBe(true);
-    expect(await driver.getButtonText()).toEqual('Click me!');
-  });
-
-  it('should increment', async () => {
-    const { driver } = render(<Animate />);
-
-    await driver.clickButton();
-    await driver.clickButton();
-
-    expect(await driver.getCountText()).toEqual(
-      'You clicked this button even number (2) of times',
-    );
-  });
-
-  it('should allow changing the button text', async () => {
-    const { driver } = render(<Animate buttonText="Press me" />);
-
-    expect(await driver.getButtonText()).toEqual('Press me');
   });
 });
